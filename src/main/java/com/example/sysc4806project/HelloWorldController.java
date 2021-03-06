03f7a36b03da7dce0255a572149990d8e2edd0cc
sandeeplayam <sandeeplayam@cmail.carleton.ca>
2021-03-06 18:06:08 -0500

    First commit for the project, where springboot project was initialized using spring initializr

---
+++ src/main/java/com/example/sysc4806project/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.sysc4806project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
