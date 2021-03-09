232c6c3645a3f1baac8cf4c68657878c25b6dc4d
sandeeplayam <sandeeplayam@cmail.carleton.ca>
2021-03-08 20:04:30 -0500

    Implemented registration of users, and partially implemented login

---
+++ src/main/java/com/sysc4806project/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sysc4806project.repositories;
+
+import com.sysc4806project.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
