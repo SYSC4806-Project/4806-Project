61ec54c792d26b3f40ad6f9b6b6b250b3b38126d
Danish <danishirfan3@gmail.com>
2021-03-11 19:03:53 -0500

    Updated Home Page and fixed bug where app always went to login

--- src/main/java/com/sysc4806project/controllers/DashboardController.java
+++ src/main/java/com/sysc4806project/controllers/DashboardController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+public class DashboardController {
 
-public class AppController {
-
-    @GetMapping("/")
-    public String index () {
-        return "index";
+    @GetMapping
+    public String dashboard () {
+        return "dashboard";
     }
 }
