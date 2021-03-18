58ebf539b2f72605fb6fcf58b0947c86585db9b9
sandeeplayam <sandeeplayam@cmail.carleton.ca>
2021-03-18 18:44:28 -0400

    Making changes for application to work on heroku

--- src/main/java/com/sysc4806project/controllers/DashboardController.java
+++ src/main/java/com/sysc4806project/controllers/DashboardController.java
@@ -1,10 +1,10 @@
 package com.sysc4806project.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DashboardController {
 
     @GetMapping
--- src/main/java/com/sysc4806project/controllers/LoginController.java
+++ src/main/java/com/sysc4806project/controllers/LoginController.java
@@ -1,10 +1,10 @@
 package com.sysc4806project.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController {
 
