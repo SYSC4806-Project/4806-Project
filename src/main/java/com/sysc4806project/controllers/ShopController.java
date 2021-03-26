99190a7fb2dffbfd1170fd6bcd751616aa353fe5
Simon <simonyacoub@cmail.carleton.ca>
2021-03-25 22:16:33 -0400

    Shops Page with seperate Cards

--- src/main/java/com/sysc4806project/controllers/ShopController.java
+++ src/main/java/com/sysc4806project/controllers/ShopController.java
@@ -1,23 +1,27 @@
 package com.sysc4806project.controllers;
 
 import com.sysc4806project.models.Shop;
+import com.sysc4806project.models.User;
 import com.sysc4806project.repositories.ShopRepository;
 import com.sysc4806project.services.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping(path="/shops")
 public class ShopController {
     @Autowired
     private ShopService shopService;
 
-    @GetMapping
-    public String shops() {
+    @GetMapping("/shops")
+    public String getShopsPage(Model model) {
+        model.addAttribute("shops", shopService.getAllShops());
         return "shops";
     }
 
