a50798a59a09f140903f1cbb732bd5782e7cc674
brougy <55706278+brougy@users.noreply.github.com>
2021-04-07 21:34:05 -0400

    Products for Users
    Any shop's Products can be viewed by users.

--- src/main/java/com/sysc4806project/controllers/ShopController.java
+++ src/main/java/com/sysc4806project/controllers/ShopController.java
@@ -1,29 +1,66 @@
 package com.sysc4806project.controllers;
 
+import com.sysc4806project.dto.ProductDTO;
+import com.sysc4806project.models.Product;
 import com.sysc4806project.models.Shop;
 import com.sysc4806project.models.User;
 import com.sysc4806project.repositories.ShopRepository;
+import com.sysc4806project.services.ProductService;
 import com.sysc4806project.services.ShopService;
+import com.sysc4806project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ShopController {
     @Autowired
     private ShopService shopService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/shops")
     public String getShopsPage(Model model) {
         model.addAttribute("shops", shopService.getAllShops());
         return "shops";
     }
+
+    @GetMapping("/shop/{id}")
+    public String getShopProductsPage(@PathVariable Long id, Model model) {
+        model.addAttribute("products", productService.getAllProductsWithinShop(id)); //this id is shop id
+        model.addAttribute("shop", shopService.getShopById(id).get());
+        return "products";
+    }
+
+    @GetMapping("/shop/{id}/{productId}")
+    public String getProductPage(@PathVariable Long id,@PathVariable Long productId, Model model) {
+        Product product = productService.getProductById(productId);
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(product.getId());
+        productDTO.setName(product.getName());
+        productDTO.setPrice(product.getPrice());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setImageName(product.getImageName());
+        productDTO.setInventoryNum(product.getInventoryNum());
+        System.out.println("ProductDTO update " + productDTO.getId());
+        model.addAttribute("productDTO", productDTO);
+        model.addAttribute("shop", shopService.getShopById(id).get());
+        return "product";
+    }
+
+    @PostMapping("/shop/{id}/{productId}")
+    public String postProductPage(@PathVariable Long id,@PathVariable Long productId, Model model) {
+
+        return "redirect:/shop/{id}";
+    }
+
 
 
 }
