b2c67795f0b607aeb849dc81713279ef873b1223
brougy <55706278+brougy@users.noreply.github.com>
2021-04-09 00:49:47 -0400

    Update MerchantControllerTest.java
    Added a few more integration tests for Merchant Controller end points.

--- src/test/java/com/sysc4806project/IntegrationTests/MerchantControllerTest.java
+++ src/test/java/com/sysc4806project/IntegrationTests/MerchantControllerTest.java
@@ -1,6 +1,7 @@
 package com.sysc4806project.IntegrationTests;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sysc4806project.controllers.AppController;
 import com.sysc4806project.models.Product;
 import com.sysc4806project.models.Shop;
@@ -69,10 +70,13 @@ public class MerchantControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @After
     public void tearDown()  {
 
+        System.out.println("After Baby");
         userRepository.deleteAll();
     }
 
@@ -83,6 +87,56 @@ public class MerchantControllerTest {
                 .andExpect(content().string(containsString("Welcome")))
                 .andExpect(content().contentType("text/html;charset=UTF-8"));
     }
+
+    @Test
+    @WithMockUser
+    public void testGetShops() throws Exception {
+
+        User user = new User("user","pass","f","l");
+        userRepository.save(user);
+
+        System.out.println("User "+user.getId());
+        System.out.println("Repo "+userRepository.findById(user.getId()).get().getId());
+
+
+        this.mockMvc.perform(get("/merchant/shops")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Shop #")))
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
+    }
+
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testMerchantGetProducts() throws Exception {
+
+        User user = new User("username","pass","f","l");
+        userRepository.save(user);
+
+        System.out.println("User "+user.getId());
+        System.out.println("Repo "+userRepository.findById(user.getId()).get().getId());
+
+        Shop shop = new Shop("Shop",user, new ArrayList<>());
+        shopRepository.save(shop);
+
+        System.out.println("Shop "+shop.getId());
+        System.out.println("Repo "+shopRepository.findByName("Shop").getId());
+
+
+        Product product = new Product("prod",shop,2,"desc","img",10);
+        productRepository.save(product);
+
+        System.out.println("Product "+product.getId());
+        System.out.println("Repo "+productRepository.getById(product.getId()).getId());
+
+
+        this.mockMvc.perform(get("/merchant/products/{id}",3))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Products")))
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
+
+
+    }
+
 
 
     @Test
@@ -121,6 +175,167 @@ public class MerchantControllerTest {
 
         assertThat(shop.getName()).isEqualTo("MyShop");
         assertThat(shop.getCategoryList()).contains("sports");
+
+    }
+
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testMerchantGetProductAdd() throws Exception {
+
+        User user = new User("user","pass","f","l");
+        userRepository.save(user);
+
+        Shop shop = new Shop("Shop",user, new ArrayList<>());
+        //shop.setId(1L);
+        shopRepository.save(shop);
+
+        System.out.println("Shop "+shop.getId());
+        System.out.println("Repo "+shopRepository.findByName("Shop").getId());
+
+        this.mockMvc.perform(get("/merchant/products/add/{id}",8)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Add a new Product")))
+
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
+    }
+
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testMerchantPostProductAdd() throws Exception{
+        System.out.println("Content " +content());
+        System.out.println("String  "+ content().toString());
+        System.out.println("Model  "+ model());
+
+        User user = new User("username","pass","f","l");
+        userRepository.save(user);
+
+        System.out.println("User "+user.getId());
+        System.out.println("Repo "+userRepository.findById(user.getId()).get().getId());
+
+        Shop shop = new Shop("Shop",user, new ArrayList<>());
+        //shop.setId(1L);
+        shopRepository.save(shop);
+
+        System.out.println("Shop "+shop.getId());
+        System.out.println("Repo "+shopRepository.findByName("Shop").getId());
+
+
+        Product product = new Product("prod",shop,2,"desc","img",10);
+        productRepository.save(product);
+
+        System.out.println("Product "+product.getId());
+        System.out.println("Repo "+productRepository.getById(product.getId()).getId());
+
+        MockMultipartFile file = new MockMultipartFile("file", "original_filename.ext", null, "data".getBytes());
+
+
+        this.mockMvc.perform(multipart("/merchant/products/add/{id}",3).file(file).with(csrf()).secure( true )
+                .param("id", "1")
+                .param("name", "Ball")
+                .param("productImage","file"))
+                .andDo(print())
+        //.andExpect(redirectedUrl("/merchant/products/1"))
+        ;
+
+        product = productRepository.getById(2L);
+        System.out.println("Added Product "+product);
+        System.out.println("Product name: "+product.getName()+" Id: "+ product.getId());
+
+        assertThat(product.getName()).isEqualTo("prod");
+        assertThat(product.getId()==2);
+
+    }
+
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testGETDeleteProduct() throws Exception {
+
+        User user = new User("username","pass","f","l");
+        userRepository.save(user);
+
+        System.out.println("User "+user.getId());
+        System.out.println("Repo "+userRepository.findById(user.getId()).get().getId());
+
+        Shop shop = new Shop("Shop",user, new ArrayList<>());
+        shopRepository.save(shop);
+
+        System.out.println("Shop "+shop.getId());
+        System.out.println("Repo "+shopRepository.findByName("Shop").getId());
+
+        Product product = new Product("prod",shop,2,"desc","img",10);
+        productRepository.save(product);
+
+        System.out.println("Product "+product.getId());
+        System.out.println("Repo "+productRepository.getById(product.getId()).getId());
+
+
+        this.mockMvc.perform(get("/merchant/products/delete/{id}/{productId}",6,3))
+                .andDo(print())
+                .andExpect(redirectedUrl("/merchant/products/6"));
+
+        assertThat(productRepository.findById(3L)==null);
+
+    }
+
+
+
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testGETDeleteShop() throws Exception {
+
+        User user = new User("username","pass","f","l");
+        userRepository.save(user);
+
+        System.out.println("User "+user.getId());
+        System.out.println("Repo "+userRepository.findById(user.getId()).get().getId());
+
+        Shop shop = new Shop("Shop",user, new ArrayList<>());
+        shopRepository.save(shop);
+
+        System.out.println("Shop "+shop.getId());
+        System.out.println("Repo "+shopRepository.findByName("Shop").getId());
+
+
+        this.mockMvc.perform(get("/merchant/shops/delete/{id}",9))
+                .andDo(print())
+                .andExpect(redirectedUrl("/merchant/shops"));
+
+        assertThat(shopRepository.findByName("Shop")==null);
+
+    }
+
+
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testMerchantProductUpdate() throws Exception {
+
+        User user = new User("username","pass","f","l");
+        userRepository.save(user);
+
+        System.out.println("User "+user.getId());
+        System.out.println("Repo "+userRepository.findById(user.getId()).get().getId());
+
+        Shop shop = new Shop("Shop",user, new ArrayList<>());
+        //shop.setId(1L);
+        shopRepository.save(shop);
+
+        System.out.println("Shop "+shop.getId());
+        System.out.println("Repo "+shopRepository.findByName("Shop").getId());
+
+        Product product = new Product("prod",shop,2,"desc","img",10);
+        productRepository.save(product);
+
+        System.out.println("Product "+product.getId());
+        System.out.println("Repo "+productRepository.getById(product.getId()).getId());
+
+
+        this.mockMvc.perform(get("/merchant/products/update/{id}",4)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Update")))
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
 
     }
 
@@ -171,11 +386,11 @@ public class MerchantControllerTest {
         System.out.println("Shop name: "+shop.getName()+" List: "+ shop.getCategoryList());
 
 
-        this.mockMvc.perform(post("/merchant/shops/update/{id}",3).with(csrf()).secure( true )
+        this.mockMvc.perform(post("/merchant/shops/update/{id}",4).with(csrf()).secure( true )
                 .param("name", "MyShop")
                 .param("category", ("sports")))
                 .andDo(print())
-                .andExpect(redirectedUrl("/merchant/shops/update/3"));
+                .andExpect(redirectedUrl("/merchant/shops/update/4"));
 
         System.out.println("NEW name: "+shop.getName()+" List: "+ shop.getCategoryList());
 
